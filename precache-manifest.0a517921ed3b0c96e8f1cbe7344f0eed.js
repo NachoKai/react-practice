1f73751d1bf9361f66bb1e4629b756d7a5b159c1
NachoKai <nachokai@outlook.com>
2020-05-10 14:21:10 -0300

    Updates

---
+++ precache-manifest.0a517921ed3b0c96e8f1cbe7344f0eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94da2160216f0b3d4c80f5cd0cc30f59",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "b6e588fb3aefdaa55561",
+    "url": "/react-practice/static/css/main.1083b18a.chunk.css"
+  },
+  {
+    "revision": "119e62a3af708265a03d",
+    "url": "/react-practice/static/js/2.551665c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-practice/static/js/2.551665c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e588fb3aefdaa55561",
+    "url": "/react-practice/static/js/main.ecaf0a31.chunk.js"
+  },
+  {
+    "revision": "5ff957751f297bf343a7",
+    "url": "/react-practice/static/js/runtime-main.9f44a0aa.js"
+  }
+]);
